4691cae30cef7c8865e623acfbeca498f8ef7e94
CezaryS8 <czarek.skorupski2001@gmail.com>
2023-05-17 19:15:49 +0200

    add initializing data in data.sql

---
+++ fd-server/src/main/resources/data.sql
@@ -0,0 +1,36 @@
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (1, 10000.00, 'mbank', 1, 10, 'STALA', true, '2023-05-01', 'Lokata 1', '2023-01-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (2, 20000.00, 'mbank', 1, 10, 'STALA', true, '2022-05-01', 'Lokata 2', '2022-01-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (3, 30000.00, 'mbank', 1, 10, 'STALA', true, '2023-03-01', 'Lokata 3', '2023-01-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (4, 40000.00, 'mbank', 1, 10, 'STALA', true, '2023-02-01', 'Lokata 4', '2023-02-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (5, 50000.00, 'PKO', 1, 10, 'STALA', true, '2023-07-01', 'Lokata 5', '2023-02-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (6, 10000.00, 'PKO', 1, 10, 'STALA', true, '2023-06-01', 'Lokata 6', '2023-03-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (7, 10000.00, 'PKO', 1, 10, 'STALA', true, '2023-04-01', 'Lokata 7', '2023-03-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (8, 20000.00, 'PKO', 1, 10, 'STALA', true, '2023-05-01', 'Lokata 8', '2023-04-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (9, 10000.00, 'PKO', 1, 10, 'STALA', true, '2023-08-01', 'Lokata 9', '2023-04-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.deposit (id, amount, bank, currency_id, interest, interest_type, is_active, maturity_date, name, opening_date, profit, tax, username)
+VALUES (10, 10000.00, 'PKO', 1, 10, 'STALA', true, '2023-09-01', 'Lokata 10', '2023-05-01', 0.00, 0.00, 'czarek@wp.pl');
+
+INSERT INTO public.user_cryptocurrency (id, amount, currency_id, name, opening_date, username)
+VALUES (1, 100.00, 1, 'bitcoin', '2022-05-03', 'czarek@wp.pl');
+
+INSERT INTO public.user_cryptocurrency (id, amount, currency_id, name, opening_date, username)
+VALUES (2, 10.00, 1, 'Bitcoin', '2022-05-03', 'czarek@wp.pl');
+
